59ac394a672d1e5363d46073a95211a06c959ea4
Ahmed Alneyadi <aalneyadi1@mail.csuchico.edu>
2021-03-12 16:14:39 +0000

    code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,61 @@
-//Author: PUT YOUR NAME HERE
+//Author:  Aalneyadi1
+
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <array>
+#include <random>
+#include <chrono>
+
 using namespace std;
+
+const string BB = u8"\033[2J\033[1;1H";
 
 int main()
 {
+  int totalColor;
+  string fName;
+  string name;
+  string color;
+  fstream reader;
+  array<string, 5> clrPalt {"#0000CF","#FFFFFF",  "#E69F00", "#56B4E9", "#009E73"};
+
+    // To shuffle the colour and use random number num with the help of the system clock
+  int num = chrono::system_clock::now().time_since_epoch().count();
+  shuffle (clrPalt.begin(), clrPalt.end(), default_random_engine(num));
+
+  cout << "Please enter the file name to save color palette: \n";
+  getline(cin, name);
+  fName = name + ".txt"; //Save name as txt file type
+
+  cout << "How many colours do you need?  \n";
+  cin >> totalColor;
+
+  //To save total number of colour from palette to the text file
+  reader.open(fName, ios::out);
+  if (reader.is_open()) {
+		cout << "File has been created successfully!\n";
+		// To save colour from palette and save in the text file.
+    for (int i = 1; i <= totalColor; i++){
+      reader << clrPalt[i] << endl;
+    }
+  }
+	else {
+		cout << "File could not created!";
+	}
+  reader.close();
+
+  //Output the colors in the file created
+  reader.open(fName, ios::in);
+  if(reader.is_open()){
+    cout << BB;
+    cout << fName << " contains  " << totalColor << " colors \n";
+    while(reader.eof() != true){
+      getline(reader, color);
+      cout << color << endl;
+    }
+ }
+
 
   return 0;
 }
