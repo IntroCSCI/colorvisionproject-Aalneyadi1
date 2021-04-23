c50fbe415b88e8ce331f658bddaedeaf3c452f6f
Aalneyadi1 <77987820+Aalneyadi1@users.noreply.github.com>
2021-04-23 00:48:50 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,56 +6,83 @@
 #include <array>
 #include <random>
 #include <chrono>
-
+#include <string>
 using namespace std;
 
 const string BB = u8"\033[2J\033[1;1H";
 
+
+//This function is used to save total number of colors from palette to the text file
+//This functions recieve pass-by-value and pass-by-reference params
+void saveNumber(string name, int &totalNumber, array<string,5> clrpalt)
+{
+    fstream reader;
+
+    //To save total number of colour from palette to the text file
+    reader.open(name, ios::out);
+    if (reader.is_open()) {
+        cout << "File has been created successfully!\n";
+        // To save colour from palette and save in the text file.
+        if (totalNumber >= 5)
+        {
+            cout << "Number are Bigger than Here";
+        }
+        else
+        {
+            for (int i = 1; i <= totalNumber; i++) {
+                reader << clrpalt[i] << endl;
+            }
+        }
+    }
+    else {
+        cout << "File could not created!";
+    }
+    reader.close();
+}
+
+
+//This function is used to Output colors in file created.
+//This functions recieve pass-by-value and pass-by-reference params
+void Display(string name,int &totalNumber,string &color)
+{
+    fstream reader;
+
+    //Output the colors in the file created
+    reader.open(name, ios::in);
+    if (reader.is_open()) {
+        cout << BB;
+        cout << name << " contains  " << totalNumber << " colors \n";
+        while (reader.eof() != true) {
+            getline(reader, color);
+            cout << color << endl;
+        }
+    }
+}
+
 int main()
 {
-  int totalColor;
-  string fName;
-  string name;
-  string color;
-  fstream reader;
-  array<string, 5> clrPalt {"#0000CF","#FFFFFF",  "#E69F00", "#56B4E9", "#009E73"};
+    int totalColor;
+    string fName;
+    string name;
+    string color;
+    fstream reader;
+    array<string, 5> clrPalt{ "#0000CF","#FFFFFF",  "#E69F00", "#56B4E9", "#009E73" };
 
     // To shuffle the colour and use random number num with the help of the system clock
-  int num = chrono::system_clock::now().time_since_epoch().count();
-  shuffle (clrPalt.begin(), clrPalt.end(), default_random_engine(num));
+    int num = chrono::system_clock::now().time_since_epoch().count();
+    shuffle(clrPalt.begin(), clrPalt.end(), default_random_engine(num));
 
-  cout << "Please enter the file name to save color palette: \n";
-  getline(cin, name);
-  fName = name + ".txt"; //Save name as txt file type
+    cout << "Please enter the file name to save color palette: \n";
+    std::getline(cin, name);
+    fName = name + ".txt"; //Save name as txt file type
 
-  cout << "How many colours do you need?  \n";
-  cin >> totalColor;
+    cout << "How many colours do you need?  \n";
+    cin >> totalColor;
 
-  //To save total number of colour from palette to the text file
-  reader.open(fName, ios::out);
-  if (reader.is_open()) {
-		cout << "File has been created successfully!\n";
-		// To save colour from palette and save in the text file.
-    for (int i = 1; i <= totalColor; i++){
-      reader << clrPalt[i] << endl;
-    }
-  }
-	else {
-		cout << "File could not created!";
-	}
-  reader.close();
+    saveNumber(fName, totalColor, clrPalt);
 
-  //Output the colors in the file created
-  reader.open(fName, ios::in);
-  if(reader.is_open()){
-    cout << BB;
-    cout << fName << " contains  " << totalColor << " colors \n";
-    while(reader.eof() != true){
-      getline(reader, color);
-      cout << color << endl;
-    }
- }
+    Display(fName, totalColor, color);
 
 
-  return 0;
+    return 0;
 }
